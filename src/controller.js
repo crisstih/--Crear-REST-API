4f80b54cdfce10eeb7d77f6be18f68c27363f1bf
Diaz_Cristian <crizz_diazz@hotmail.com>
2023-10-22 18:23:02 -0300

    Agregue excepciones try catch a funciones getAll y getOne

--- src/controller.js
+++ src/controller.js
@@ -4,45 +4,60 @@ class LibroController {
   //traer todo lo que tengo en la base de datos. Ej: http://localhost:3000/libros
 
   async getAll(req, res) {
-    const [result] = await pool.query("SELECT * FROM libros");
-    res.json(result);
+    try {
+      const [result] = await pool.query("SELECT * FROM libros");
+      res.json(result);
+    } catch (error) {
+      console.error('Error al obtener todos los libros: ', error);
+      res.status(500).json({ message: 'Error en el servidor al obtener todos los libros' });
+    }
   }
 
-  //obtener un libro por id. Ej: http://localhost:3000/libros/3
+  //obtener un libro por id. Ej: http://localhost:3000/libro/3
   async getOne(req, res, id) {
-    const [result] = await pool.query("SELECT * FROM libros WHERE id = ?", [
-      id,
-    ]);
-    if (result.length === 0) {
-      return res.status(404).json({ message: "Libro no encontrado" });
+    try {
+      const [result] = await pool.query("SELECT * FROM libros WHERE id = ?", [id]);
+      if (result.length === 0) {
+        return res.status(404).json({ message: "Libro no encontrado" });
+      }
+      res.json(result[0]);
+    } catch (error) {
+      console.error('Error al obtener el libro por ID: ', error);
+      res.status(500).json({ message: 'Error en el servidor al obtener el libro por ID' });
     }
-    res.json(result[0]);
   }
 
   //se coloca http://localhost:3000/libro luego en postman se pone los datos del libro a insertar. ej: {"nombre": "Don Quijote","autor": "Miguel de Cervantes","categoria": "Novela Clasica","año-publicacion": "1605","ISBN": "978-84-376"}
   async add(req, res) {
-    const libro = req.body;
-    const [result] = await pool.query(
-      "INSERT INTO Libros(nombre, autor, categoria, `año-publicacion`, ISBN) VALUES (?, ?, ?, ?, ?)",
-      [
-        libro.nombre,
-        libro.autor,
-        libro.categoria,
-        `libro.año-publicacion`,
-        libro.ISBN,
-      ]
-    );
-    res.json({ "Id insertado": result.insertId });
-  }
+      const libro = req.body;
+      const [result] = await pool.query(
+        "INSERT INTO Libros(nombre, autor, categoria, `año-publicacion`, ISBN) VALUES (?, ?, ?, ?, ?)",
+        [
+          libro.nombre,
+          libro.autor,
+          libro.categoria,
+          `libro.año-publicacion`,
+          libro.ISBN,
+        ]
+      );
+      res.json({ "Id insertado": result.insertId });
+
+    }
+  
 
   //Se coloca http://localhost:3000/libro en postman luego se ingresa el isbn a eliminar
   async delete(req, res) {
-    const libro = req.body;
-    const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [
-      libro.ISBN,
-    ]);
-    res.json({ "Registros eliminados": result.affectedRows });
+    try {
+      const libro = req.body;
+      const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [libro.ISBN]);
+      if (result.affectedRows === 0) {
+        return res.status(404).json({ message: "Libro no encontrado" });
+      }
+      res.json({ "Libro eliminado": result.affectedRows });
+    } catch (error) {
+      console.error('Error al eliminar el libro: ', error);
+      res.status(500).json({ message: 'Error en el servidor al eliminar el libro' });
+    }
   }
 }
-
 export const libro = new LibroController();
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import {libro} from './controller.js';
 export const router = Router();
 
 router.get('/libros', libro.getAll);
-router.get('/libros/:id', async (req, res) => {
+router.get('/libro/:id', async (req, res) => {
     const libroId = req.params.id;
     await libro.getOne(req, res, libroId);
 });
