8def17e79a09ead4e3900d752786444592b22db8
Diaz_Cristian <crizz_diazz@hotmail.com>
2023-10-25 13:42:38 -0300

    Implementacion de excepciones en getaAll y update

--- src/controller.js
+++ src/controller.js
@@ -8,75 +8,95 @@ class LibroController {
       const [result] = await pool.query("SELECT * FROM libros");
       res.json(result);
     } catch (error) {
-      console.error('Error al obtener todos los libros: ', error);
-      res.status(500).json({ message: 'Error en el servidor al obtener todos los libros' });
+      console.error("Error al obtener todos los libros: ", error);
+      res
+        .status(500)
+        .json({ message: "Error en el servidor al obtener todos los libros" });
     }
   }
 
   //obtener un libro por id. Ej: http://localhost:3000/libro/3
   async getOne(req, res, id) {
     try {
-      const [result] = await pool.query("SELECT * FROM libros WHERE id = ?", [id]);
+      const [result] = await pool.query("SELECT * FROM libros WHERE id = ?", [
+        id,
+      ]);
       if (result.length === 0) {
         return res.status(404).json({ message: "Libro no encontrado" });
       }
       res.json(result[0]);
     } catch (error) {
-      console.error('Error al obtener el libro por ID: ', error);
-      res.status(500).json({ message: 'Error en el servidor al obtener el libro por ID' });
+      console.error("Error al obtener el libro por ID: ", error);
+      res
+        .status(500)
+        .json({ message: "Error en el servidor al obtener el libro por ID" });
     }
   }
 
-  //se coloca http://localhost:3000/libro luego en postman se pone los datos del libro a insertar. 
+  //se coloca http://localhost:3000/libro luego en postman se pone los datos del libro a insertar.
   //ej: {"nombre": "Don Quijote","autor": "Miguel de Cervantes","categoria": "Novela Clasica","año-publicacion": "1605","ISBN": "978-84-376"}
 
   async add(req, res) {
+    const libro = req.body;
+    try {
+    const [result] = await pool.query(
+      "INSERT INTO Libros(nombre, autor, categoria, `año-publicacion`, ISBN) VALUES (?, ?, ?, ?, ?)",
+      [
+        libro.nombre,
+        libro.autor,
+        libro.categoria,
+        `libro.año-publicacion`,
+        libro.ISBN,
+      ]
+    );
+    res.json({ "Id insertado": result.insertId });
+  } catch (error) {
+    console.error("Error al agregar un nuevo libro: ", error);
+    res.status(500).json({ error: "Ocurrió un error al agregar el libro." });
+}
+
+}
+  //Se coloca http://localhost:3000/libro en postman luego se ingresa el isbn a eliminar
+  async delete(req, res) {
+    try {
       const libro = req.body;
+      const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [
+        libro.ISBN,
+      ]);
+      if (result.affectedRows === 0) {
+        return res.status(404).json({ message: "Libro no encontrado" });
+      }
+      res.json({ "Libro eliminado": result.affectedRows });
+    } catch (error) {
+      console.error("Error al eliminar el libro: ", error);
+      res
+        .status(500)
+        .json({ message: "Error en el servidor al eliminar el libro" });
+    }
+  }
+
+  //actualizar un libro colocando datos y obligatoriamente el id en postman
+  async update(req, res) {
+    const libro = req.body;
+    try {
       const [result] = await pool.query(
-        "INSERT INTO Libros(nombre, autor, categoria, `año-publicacion`, ISBN) VALUES (?, ?, ?, ?, ?)",
+        "UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), `año-publicacion`=(?), ISBN=(?) WHERE id=(?)",
         [
           libro.nombre,
           libro.autor,
           libro.categoria,
           `libro.año-publicacion`,
           libro.ISBN,
+          libro.id,
         ]
       );
-      res.json({ "Id insertado": result.insertId });
-
-    }
-  
-
-  //Se coloca http://localhost:3000/libro en postman luego se ingresa el isbn a eliminar
-  async delete(req, res) {
-    try {
-      const libro = req.body;
-      const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [libro.ISBN]);
-      if (result.affectedRows === 0) {
-        return res.status(404).json({ message: "Libro no encontrado" });
-      }
-      res.json({ "Libro eliminado": result.affectedRows });
+      res.json({ "Libros actualizados": result.changedRows });
     } catch (error) {
-      console.error('Error al eliminar el libro: ', error);
-      res.status(500).json({ message: 'Error en el servidor al eliminar el libro' });
+      console.error("Error al actualizar registros: ", error);
+      res
+        .status(500)
+        .json({ error: "Ocurrió un error al actualizar los registros." });
     }
   }
-
-
-
-  async update(req, res){
-    const libro = req.body;
-    const [result] = await pool.query("UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), `año-publicacion`=(?), ISBN=(?) WHERE id=(?)",[
-      libro.nombre,
-      libro.autor,
-      libro.categoria,
-      `libro.año-publicacion`,
-      libro.ISBN,
-      libro.id
-    ]);
-    res.json({"Registros actualizados": result.changedRows});
-  }
-
-
 }
 export const libro = new LibroController();
